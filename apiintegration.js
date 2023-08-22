074e46409f7b1fd83f1ddd84cea362899463a9a8
Neth <69278687+0neth@users.noreply.github.com>
2023-08-22 00:39:39 -0300

    Add files via upload

---
+++ apiintegration.js
@@ -0,0 +1,7 @@
+
+function RandomDog(params) {
+    fetch("https://dog.ceo/api/breeds/"
+                        + "image/random")
+.then(response=> response.json())
+.then(data => imgDog.src = data.message);
+}
